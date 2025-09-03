a227f5003eb2cea1956e670b5eefa37d41480260
Tanmaykaturi <159781384+Tanmaykaturi@users.noreply.github.com>
2025-09-03 07:51:27 +0530

    Delete app5.py

--- app5.py
+++
@@ -1,8 +0,0 @@
-def greet_user(name: str) -> str:
-    """Return a greeting for the given user."""
-    return f"Hello, {name}!"
-
-
-if __name__ == "__main__":
-    user = "Main Branch"
-    print(greet_user(user))
